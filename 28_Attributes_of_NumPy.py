db489c54b895a28bc4c83c7fb6b08568fe4e5487
havenmini <haven.mini3@gmail.com>
2024-10-27 16:36:07 +0545

    modify 28 file

--- 28_Attributes_of_NumPy.py
+++ 28_Attributes_of_NumPy.py
@@ -5,3 +5,16 @@ size: एरेमा कतिवटा तत्वहरू छन् ।
 dtype: एरेमा प्रयोग गरिएको डेटा प्रकार ।
 itemsize: एरे कति byte साईजको छ ।
 '''
+
+import numpy as nd
+listone = [10, 20, 30, 40, 50, 60],
+listtwo = [10, 20, 30, 40, 50, 60], [1, 2, 3, 4, 5, 6]
+listthree = [[10, 20, 30, 40, 50, 60]], [[1, 2, 3, 4, 5, 6]], [[2, 4, 6, 8, 10, 12]]
+
+arrayone = nd.array(listone)
+arraytwo = nd.array(listtwo)
+arraythree = nd.array(listthree)
+
+print(arrayone.ndim)
+print(arraytwo.ndim)
+print(arraythree.ndim)
